09236fa4f475644618896a20bc275f0d7234fee4
itsdominiclike <70446156+itsdominiclike@users.noreply.github.com>
2025-03-07 11:22:10 +0000

    2 paddle objects, detect input, move left paddle with inputs

--- main.py
+++ main.py
@@ -2,22 +2,25 @@ from turtle import Turtle, Screen
 from paddle import Paddle
 from scoreboard import Scoreboard
 
-paddle = Paddle()
+l_paddle = Paddle("turquoise", -350)
+r_paddle = Paddle("red", 350)
 screen = Screen()
+
+
+# Detect input
+screen.listen()
+screen.onkeypress(l_paddle.up, "w")
+screen.onkeypress(l_paddle.down, "s")
 
 
 screen.setup(800, 800)
 screen.bgcolor("black")
 screen.title("My Pong Game")
-screen.tracer(0)
 
-
-
-#  this is a test commit
-
-
-
-
+# playing = True
+#
+# # while playing:
+# #     pass
 
 
 screen.exitonclick()
--- paddle.py
+++ paddle.py
@@ -1,16 +1,39 @@
 import turtle
 from turtle import Turtle
+
+UP = 90
+DOWN = 270
+MOVE_DISTANCE = 20
+X_POS = -350
+
+
 class Paddle():
-    def __init__(self):
+    def __init__(self, color, x_pos):
+        self.color = color
+        self.x_pos = x_pos
         self.create_paddle()
 
+
     def create_paddle(self):
-        tubby = turtle.Turtle()
-        tubby.penup()
-        tubby.goto(-350, 0)
-        tubby.color("turquoise")
-        tubby.shape("square")
-        tubby.shapesize(5,1, None)
+        self.tubby = turtle.Turtle()
+        self.tubby.penup()
+        self.tubby.goto(self.x_pos, 0)
+        self.tubby.color(self.color)
+        self.tubby.shape("square")
+        self.tubby.shapesize(5,1, None)
+        self.new_y = 0
+
+
+    def up(self):
+        self.new_y += MOVE_DISTANCE
+        self.tubby.goto(X_POS, self.new_y)
+
+
+    def down(self):
+        self.new_y -= MOVE_DISTANCE
+        self.tubby.goto(X_POS, self.new_y)
+
+
 
 
 
